ec5e16c1136dfd46fd2030e224f7921994ec0d9e
LookForwardPersistence <fqhua_5@sina.cn>
2020-04-09 22:51:14 +0800

    fix

--- src/main/java/com/dawn/controller/DawnController.java
+++ src/main/java/com/dawn/controller/DawnController.java
@@ -1,7 +1,20 @@
 package com.dawn.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by Dawn on 2020-04-05.
  */
+@Controller
+@RequestMapping("/dawn")
 public class DawnController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printDawn(Model model){
+        model.addAttribute("message","Come on Dawn!!!");
+        return "dawn";
+    }
 }
---
+++ src/main/resources/WebContent/WEB-INF/dawn.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html; charset=utf-8" %>
+
+<html>
+<head>
+    <title>Dawn web</title>
+
+</head>
+<body>
+<h1>${message}</h1>
+</body>
+</html>
